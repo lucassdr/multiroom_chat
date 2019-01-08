95aa74d081fb36cf03172aedfc75eb3b9a113fa8
Lucas Sodré <lucassodre1996@gmail.com>
2019-01-07 23:33:11 -0200

    Configuração para o socket.io ouvir na mesma porta

--- app.js
+++ app.js
@@ -2,6 +2,9 @@
 var app = require('./config/server');
 
 /* parametrizar a porta que a aplicação responderá a aplicação  */
-app.listen(3000, function(){
+var server = app.listen(3000, function(){
     console.log('Servidor multichat online');
 });
+
+/* fazendo o socket.io possa ouvir na porta 3000 */
+require('socket.io').listen(server);
